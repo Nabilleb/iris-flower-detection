4c4abee4fbe3189280eb2db44b63a11d7ad3ab1a
Nabilleb <nabilgreen500@gmail.com>
2026-01-12 15:26:56 +0200

    user can see iris examples

--- client/app.js
+++ client/app.js
@@ -9,3 +9,24 @@ const predLabelEl = document.getElementById("predLabel");
 const pSetosaEl = document.getElementById("pSetosa");
 const pVersicolorEl = document.getElementById("pVersicolor");
 const pVirginicaEl = document.getElementById("pVirginica");
+
+
+function setInputs(a, b, c, d) {
+  document.getElementById("sepal_length").value = a;
+  document.getElementById("sepal_width").value = b;
+  document.getElementById("petal_length").value = c;
+  document.getElementById("petal_width").value = d;
+}
+
+
+document.getElementById("fillSetosa").addEventListener("click", () => {
+  setInputs(5.1, 3.5, 1.4, 0.2);
+});
+
+document.getElementById("fillVersicolor").addEventListener("click", () => {
+  setInputs(6.0, 2.7, 4.5, 1.5);
+});
+
+document.getElementById("fillVirginica").addEventListener("click", () => {
+  setInputs(6.5, 3.0, 5.5, 2.0);
+});
