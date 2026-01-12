0010102544d00ea58b5026ec7c50a7802ef095c4
Nabilleb <nabilgreen500@gmail.com>
2026-01-12 15:23:05 +0200

    initalised var

---
+++ client/app.js
@@ -0,0 +1,11 @@
+const API_URL = "http://127.0.0.1:5000/predict";
+
+const form = document.getElementById("irisForm");
+const statusEl = document.getElementById("status");
+const resultEl = document.getElementById("result");
+const rawEl = document.getElementById("raw");
+
+const predLabelEl = document.getElementById("predLabel");
+const pSetosaEl = document.getElementById("pSetosa");
+const pVersicolorEl = document.getElementById("pVersicolor");
+const pVirginicaEl = document.getElementById("pVirginica");
